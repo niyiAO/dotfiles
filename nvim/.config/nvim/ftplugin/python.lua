d52adcb9ec447dc697ec9b8677572fbb62c64287
Niyi Adeosun <deniyi.adeosun@gmail.com>
2024-07-02 06:30:43 +0100

    fix python.lua, add lazy-lock.json

--- nvim/.config/nvim/ftplugin/python.lua
+++ nvim/.config/nvim/ftplugin/python.lua
@@ -1,5 +1,6 @@
-
+vim.cmd([[
 let g:python_recommended_style = 0
 setlocal noexpandtab
 setlocal tabstop=2
 setlocal shiftwidth=2
+]])
--- nvim/.config/nvim/lua/rice.lua
+++ nvim/.config/nvim/lua/rice.lua
@@ -12,12 +12,10 @@ vim.opt.listchars = {
 }
 
 vim.cmd([[
-highlight Normal guibg=none ctermbg=none
+set notermguicolors
 highlight EndOfBuffer cterm=none ctermfg=0
-highlight StatusLine cterm=bold,reverse ctermfg=9 ctermbg=0 
-highlight GitGutterAdd    guifg=#009900 ctermfg=2
-highlight GitGutterChange guifg=#bbbb00 ctermfg=3
-highlight GitGutterDelete guifg=#ff2222 ctermfg=1
+highlight StatusLine cterm=bold,reverse ctermfg=9 ctermbg=0
+highlight LineNr ctermbg=none
 ]])
 
 
