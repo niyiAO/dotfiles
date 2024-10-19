03a858552ebd9a5e816d4bb0c322443982c9be13
Niyi Adeosun <deniyi.adeosun@gmail.com>
2024-10-19 21:47:29 +0100

    add rounded border to hover and signature help handlers

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -17,6 +17,20 @@ return {
 		config = function()
 			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+			local border = "rounded"
+
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+				vim.lsp.handlers.hover, {
+					border = border
+				}
+			)
+
+			vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+				vim.lsp.handlers.signature_help, {
+					border = border
+				}
+			)
+
 			local lsps = {
 				tailwindcss = { capabilities = capabilities },
 				ts_ls = { capabilities = capabilities },
@@ -24,6 +38,7 @@ return {
 				bashls = { capabilities = capabilities },
 				yamlls = { capabilities = capabilities },
 				html = { capabilities = capabilities },
+				gopls = { capabilities = capabilities },
 				lua_ls = {
 					capabilities = capabilities,
 					on_init = function(client)
